a52c6980eff748c8b8573c9c0525d30b3462061c
pookage <pookagehayes@gmail.com>
2019-06-03 17:04:25 +0100

    sketched out start of logic to draw frames onto canvas and output to image

--- js/ui.js
+++ js/ui.js
@@ -14,8 +14,13 @@ function init(){
 			addFrame: document.getElementById("input__generator__add_frame"),
 			generate: document.getElementById("input__generator__generate"),
 			canvas: document.getElementById("output__generator__canvas"),
-			canvasInputHeight: document.getElementById("input__canvas__height"),
-			canvasInputWidth: document.getElementById("input__canvas__width")
+			canvasHeight: document.getElementById("input__canvas__height"),
+			canvasWidth: document.getElementById("input__canvas__width"),
+			repeat: document.getElementById("input__generator__repeat"),
+			delay: document.getElementById("input__generator__delay"),
+			dispose: document.getElementById("input__generator__dispose"),
+			blend: document.getElementById("input__generator__blend"),
+			image: document.getElementById("output__generator__image")
 		}
 	}
 
@@ -23,13 +28,14 @@ function init(){
 	//EVENT LISTENERS
 	//------------------------------
 	app.elements.addFrame.addEventListener("click", addFrameOption);
-	app.elements.canvasInputHeight.addEventListener("change", syncCanvasDimenions);
-	app.elements.canvasInputWidth.addEventListener("change", syncCanvasDimenions);
+	app.elements.canvasHeight.addEventListener("change", syncOutputDimensions);
+	app.elements.canvasWidth.addEventListener("change", syncOutputDimensions);
+	app.elements.generate.addEventListener("click", generateAPNG);
 
 
 	//SETUP
 	//-----------------------------
-	syncCanvasDimenions();
+	syncOutputDimensions();
 
 
 
@@ -59,19 +65,41 @@ function init(){
 		validateInputs(app.elements.frames, app.elements.addFrame, "Make sure every frame has a file!");
 		validateInputs(app.elements.app, app.elements.generate);
 	}//selectFrameFile
-	function syncCanvasDimenions(){
-		const {
-			canvasInputWidth,
-			canvasInputHeight,
-			canvas
-		} = app.elements;
+	function syncOutputDimensions(){
 
-		const width  = canvasInputWidth.value;
-		const height = canvasInputHeight.value;
+		const width  = app.elements.canvasWidth.value;
+		const height = app.elements.canvasHeight.value;
 
-		canvas.width = width;
-		canvas.height = height;
-	}//syncCanvasDimensions
+		app.elements.canvas.width  = width;
+		app.elements.canvas.height = height;
+		app.elements.image.width   = width;
+		app.elements.image.height  = height;
+	}//syncOutputDimensions
+	function generateAPNG(){
+
+		//grab everything we need from the UI
+		const repeat  = app.elements.repeat.value;
+		const delay   = app.elements.delay.value;
+		const dispose = app.elements.dispose.value;
+		const blend   = app.elements.blend.value;
+		const output  = app.elements.image;
+		const encoder = new APNGencoder(app.elements.canvas);
+
+		//configure the encoder
+		encoder.setRepeat(repeat);
+		encoder.setDelay(delay);
+		encoder.setDispose(dispose);
+		encoder.setBlend(blend);
+
+		//draw all the frames and save them ready
+		encoder.start();
+		drawFrames();
+		encoder.finish();
+
+		const base64 = bytesToBase64(encoder.stream().bin);
+
+
+	}//generateAPNG
 
 
 	//UTILS
